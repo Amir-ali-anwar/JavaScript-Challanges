4f7a23d01cdec05e84cb3a75a09d2c3df5a2155b
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2023-05-31 00:10:28 +0500

    first challange completed

--- Basic-challanges/app.js
+++ Basic-challanges/app.js
@@ -1,1 +1,18 @@
-console.log('data');
+// 1. Write a JavaScript program to display the current day and time in the following format.  
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+const currentDay=()=>{
+    const daylist= ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+    let date= new Date();
+    let today= date.getDay()
+    const timeshif= date.getHours() < 12 ? 'AM': 'PM'
+
+    console.log(`Today is: ${daylist[today]}`);
+    console.log(`Time is: ${date.getHours()} ${timeshif} ${date.getMinutes()} ${date.getMilliseconds()} `);
+    // console.log(`Today is: ${date.getMinutes()}`);
+    // console.log(`Today is: ${date.getMilliseconds()}`);
+    return date
+}
+
+currentDay()
