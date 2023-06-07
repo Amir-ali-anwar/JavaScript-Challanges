bb405cfc85eec8f3fc14931b0abe335682a99c1d
Amir Ali anwar <amiralianwar611@gmail.com>
2023-06-07 22:42:47 +0500

    challange 11 completed

--- Basic-challanges/app.js
+++ Basic-challanges/app.js
@@ -142,4 +142,13 @@ const pystring=(value)=>{
   }
   return `Py${value}`
 }
-console.log(pystring('Pyar'))
+// console.log(pystring('Pyar'))
+
+// 11. Write a JavaScript program to remove a character at the specified position in a given string and return the modified string.  
+
+const removeChar=(userstring,position)=>{
+  const newChar= userstring.substring(0,position+1)
+  return newChar
+}
+// console.log(removeChar("Python",0));
+
