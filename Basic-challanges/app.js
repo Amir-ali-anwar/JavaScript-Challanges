699352e9322d0181c509c9ce8e46542a6e290269
Amir Ali anwar <amiralianwar611@gmail.com>
2023-07-04 15:03:41 +0500

    22 problem solved with optimized code

--- Basic-challanges/app.js
+++ Basic-challanges/app.js
@@ -409,11 +409,11 @@ const csv_toArray=(data)=>{
 
 const CSV_to_JSON=(data)=>{
   const title= data.slice(0,data.indexOf('\n')).split(',')
-  return data.slice(data.indexOf('\n') + 1).split('\n').map((values)=>{
-    return values.split(',').reduce((obj,title,index)=>{
-      (obj[title] = values[index]), obj
+  return data.slice(data.indexOf('\n') + 1).split('\n').map((value)=>{
+    const singlevalues= value.split(',')
+    return title.reduce((obj, title, index)=>{
+     return {...obj,[title]:singlevalues[index]}
     },{})
   })
 }
-
 console.log(CSV_to_JSON('col1,col2\na,b\nc,d')); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
