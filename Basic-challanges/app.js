6d2dddd01ac26a928ec27c08c15d55f4f4d45384
Amir Ali anwar <amiralianwar611@gmail.com>
2023-07-04 14:26:40 +0500

    22. problem solved

--- Basic-challanges/app.js
+++ Basic-challanges/app.js
@@ -385,4 +385,35 @@ return Object.keys(obj).every(key=>obj.hasOwnProperty(key) && obj[key] === souce
 const obj1 = { age: 25, hair: 'long', beard: true };
 const obj2 = { hair: 'long', beard: true };
 
-console.log(matches(obj1, obj2)); // true
+// console.log(matches(obj1, obj2)); // true
+
+
+//21. Write a JavaScript program to convert a comma-separated value (CSV) string to a 2D array.
+
+
+const csv_toArray=(data)=>{
+  const row= data.trim("").split("\n").slice(0, data.length-1)
+  let newarray=[];
+  for (let index = 0; index < row.length; index++) {
+    const newrow= row[index].split(',')
+    newarray.push(newrow)
+    
+  }
+  return newarray
+}
+// console.log(csv_toArray('a,b\nc,d'))
+// console.log(csv_toArray('a;b\nc;d', ';'))
+// console.log(csv_toArray('John,Doe,25\nJane,Smith,30\nBob,Johnson,40'))
+
+//22. Write a JavaScript program to convert a comma-separated value (CSV) string to a 2D array of objects. The first row of the string is used as the title row.
+
+const CSV_to_JSON=(data)=>{
+  const title= data.slice(0,data.indexOf('\n')).split(',')
+  return data.slice(data.indexOf('\n') + 1).split('\n').map((values)=>{
+    return values.split(',').reduce((obj,title,index)=>{
+      (obj[title] = values[index]), obj
+    },{})
+  })
+}
+
+console.log(CSV_to_JSON('col1,col2\na,b\nc,d')); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
